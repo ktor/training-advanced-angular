56dcd59ab45e513e8e59f56df8e65a679ca396aa
Igor Hlina <srigi@srigi.sk>
2015-02-24 17:24:38 +0100

    Sources autoinjecting

--- day1/gulpfile.js
+++ day1/gulpfile.js
@@ -2,6 +2,7 @@
 'use strict';
 
 
+var del             = require('del');
 var g               = require('gulp-load-plugins')();
 var gulp            = require('gulp');
 var runSequence     = require('run-sequence');
@@ -14,10 +15,30 @@ src.app             = {toString: function() { return src + '/app' }};
 src.app.files       = src.app + '/**/*.js';
 src.index           = src + '/index.html';
 
+var dist            = {toString: function() { return 'dist' }};
+dist.index          = dist + '/index.html';
+
 var vendor          = {toString: function() { return 'bower_components' }};
 
 
 // === Develpement ===
+
+gulp.task('clean', function(cb) {
+  return del([
+    dist + '/*',
+  ], cb);
+});
+
+gulp.task('index', function() {
+  var sources = gulp.src(src.app.files, {read: false});
+
+  return gulp.src(src.index)
+    .pipe(g.inject(sources, {
+      addRootSlash: false,
+      ignorePath: '/src',
+    }))
+    .pipe(gulp.dest(''+dist));
+});
 
 gulp.task('jshint', function() {
   return gulp.src(src.app.files)
@@ -29,7 +50,7 @@ gulp.task('serve', function() {
   return g.connect.server({
     host: 'localhost',
     port: '8000',
-    root: ''+src,
+    root: [''+dist, ''+src],
     middleware: function(connect, options) {
       return [
         connect().use('/' + vendor, connect.static(''+vendor))
@@ -42,7 +63,8 @@ gulp.task('watch', function() {
   g.livereload.listen();
 
   // compile handlers
-  gulp.watch(src.app.files, ['jshint']);
+  gulp.watch(src.app.files, ['index', 'jshint']);
+  gulp.watch(src.index, ['index']);
 
   // livereload handlers
   gulp.watch([
@@ -56,7 +78,7 @@ gulp.task('watch', function() {
 
 gulp.task('default', function() {
   return runSequence(
-    ['jshint', 'serve'],
+    ['index', 'jshint', 'serve'],
     'watch'
   );
 });
