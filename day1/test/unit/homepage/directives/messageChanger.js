08920b6034b847fd7b9d8e424da8c0fd85ba6da1
Igor Hlina <srigi@srigi.sk>
2015-02-24 18:32:25 +0100

    Boilerplate test for <message-changer> directive

---
+++ day1/test/unit/homepage/directives/messageChanger.js
@@ -0,0 +1,28 @@
+/* jshint node: true, jasmine: true */
+/* global inject */
+'use strict';
+
+describe('aa.homepage.messageChanger', function() {
+  /* jshint maxstatements: 10 */
+  var $scope,
+      scope,
+      ctrl,
+      el,
+      markup = '<message-changer message="message" change-to="lorem ipsum" />';
+
+  beforeEach(module('aa.homepage'));
+  beforeEach(inject(function($rootScope, $compile) {
+    el = angular.element(markup);
+    $scope = $rootScope.$new();
+    $scope.message = 'Hello from test';
+
+    $compile(el)($scope);
+    $scope.$digest();
+    scope = el.isolateScope();
+    ctrl = el.controller('messageChanger');
+  }));
+
+
+  it('should prepare directive', function() {
+  });
+});
