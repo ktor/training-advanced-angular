ec747fbd0025cef8ace438e29e7c433cc97d9769
Igor Hlina <srigi@srigi.sk>
2015-02-24 17:12:35 +0100

    ControllerAs example

--- day1/src/app/homepage/controllers/index.js
+++ day1/src/app/homepage/controllers/index.js
@@ -3,5 +3,9 @@ angular.module('aa.homepage')
 .controller('Homepage.IndexController', function($scope) {
   'use strict';
 
+  this.setMessage = function() {
+    $scope.message = 'Lorem ipsum dolor sit amet';
+  };
+
   $scope.message = 'Hello from Homepage.IndexController';
 });
